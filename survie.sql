8c8aad4c659224dc2ed6e0f9048bfc01ddc467ef
Marvin EDORH <83826055+MarvinEdorh@users.noreply.github.com>
2021-08-13 18:58:19 +0200

    Update survie.sql

--- survie.sql
+++ survie.sql
@@ -2,32 +2,27 @@ WITH
 
 visit AS (
 SELECT fullvisitorid, MIN(date) AS date_first_visit, MAX(date) AS date_last_visit 
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` 
-WHERE _TABLE_SUFFIX BETWEEN '20160801' AND '20160831'GROUP BY fullvisitorid),
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` GROUP BY fullvisitorid),
 
 device_visit AS (
 SELECT DISTINCT fullvisitorid, date, device.deviceCategory
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` 
-WHERE _TABLE_SUFFIX BETWEEN '20160801' AND '20160831'),
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`),
 
 transactions AS (
 SELECT fullvisitorid, MAX(date) AS date_transactions, 1 AS transactions 
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga, UNNEST(ga.hits) AS hits
-WHERE _TABLE_SUFFIX BETWEEN '20160801' AND '20160831' AND hits.transaction.transactionId IS NOT NULL
-GROUP BY fullvisitorid),
+WHERE  hits.transaction.transactionId IS NOT NULL GROUP BY fullvisitorid),
 
 device_transactions AS (
 SELECT DISTINCT fullvisitorid, date, device.deviceCategory
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga, UNNEST(ga.hits) AS hits
-WHERE _TABLE_SUFFIX BETWEEN '20160801' AND '20160831' AND hits.transaction.transactionId IS NOT NULL),
+WHERE hits.transaction.transactionId IS NOT NULL),
 
 visits_transactions AS (
-
-SELECT visit.fullvisitorid, date_first_visit, date_transactions, date_last_visit , device_visit.deviceCategory AS device_last_visit, 
-device_transactions.deviceCategory AS device_transaction, IFNULL(transactions.transactions,0) AS transactions 
-
+SELECT visit.fullvisitorid, date_first_visit, date_transactions, date_last_visit , 
+       device_visit.deviceCategory AS device_last_visit, device_transactions.deviceCategory AS device_transaction, 
+       IFNULL(transactions.transactions,0) AS transactions 
 FROM visit LEFT JOIN transactions ON visit.fullvisitorid = transactions.fullvisitorid
-
 LEFT JOIN device_visit ON visit.fullvisitorid = device_visit.fullvisitorid 
 AND visit.date_last_visit = device_visit.date
 
@@ -35,12 +30,11 @@ LEFT JOIN device_transactions ON visit.fullvisitorid = device_transactions.fullv
 AND transactions.date_transactions = device_transactions.date ),
 
 mortality_table AS (
-SELECT fullvisitorid, date_first_visit,
-CASE WHEN date_transactions IS NULL THEN date_last_visit
-ELSE date_transactions  END AS date_event, 
-CASE WHEN device_transaction IS NULL THEN device_last_visit ELSE device_transaction END AS device, 
-transactions FROM visits_transactions )
+SELECT fullvisitorid, date_first_visit, 
+       CASE WHEN date_transactions IS NULL THEN date_last_visit ELSE date_transactions  END AS date_event, 
+       CASE WHEN device_transaction IS NULL THEN device_last_visit ELSE device_transaction END AS device, transactions 
+FROM visits_transactions )
 
-SELECT fullvisitorid, DATE_DIFF(PARSE_DATE('%Y%m%d',date_event),PARSE_DATE('%Y%m%d', date_first_visit),DAY) AS time, 
-device, transactions FROM mortality_table
-ORDER BY transactions DESC, time, fullvisitorid
+SELECT fullvisitorid, DATE_DIFF(PARSE_DATE('%Y%m%d',date_event), PARSE_DATE('%Y%m%d', date_first_visit),DAY) AS time, 
+       device, transactions 
+FROM mortality_table ORDER BY transactions DESC, time, fullvisitorid
