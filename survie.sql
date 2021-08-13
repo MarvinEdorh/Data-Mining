1f4163c564ac5a99595332ad8cbc3630112ea3c9
Marvin EDORH <83826055+MarvinEdorh@users.noreply.github.com>
2021-08-13 19:50:26 +0200

    Update survie.sql

--- survie.sql
+++ survie.sql
@@ -9,7 +9,7 @@ SELECT DISTINCT fullvisitorid, date, device.deviceCategory
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`),
 
 transactions AS (
-SELECT fullvisitorid, MAX(date) AS date_transactions, 1 AS transactions 
+SELECT fullvisitorid, MAX(date) AS date_transactions, 1 AS transaction
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS ga, UNNEST(ga.hits) AS hits
 WHERE  hits.transaction.transactionId IS NOT NULL GROUP BY fullvisitorid),
 
@@ -21,7 +21,7 @@ WHERE hits.transaction.transactionId IS NOT NULL),
 visits_transactions AS (
 SELECT visit.fullvisitorid, date_first_visit, date_transactions, date_last_visit , 
        device_visit.deviceCategory AS device_last_visit, device_transactions.deviceCategory AS device_transaction, 
-       IFNULL(transactions.transactions,0) AS transactions 
+       IFNULL(transactions.transaction,0) AS transaction
 FROM visit LEFT JOIN transactions ON visit.fullvisitorid = transactions.fullvisitorid
 LEFT JOIN device_visit ON visit.fullvisitorid = device_visit.fullvisitorid 
 AND visit.date_last_visit = device_visit.date
@@ -32,9 +32,8 @@ AND transactions.date_transactions = device_transactions.date ),
 mortality_table AS (
 SELECT fullvisitorid, date_first_visit, 
        CASE WHEN date_transactions IS NULL THEN date_last_visit ELSE date_transactions  END AS date_event, 
-       CASE WHEN device_transaction IS NULL THEN device_last_visit ELSE device_transaction END AS device, transactions 
+       CASE WHEN device_transaction IS NULL THEN device_last_visit ELSE device_transaction END AS device, transaction
 FROM visits_transactions )
 
 SELECT fullvisitorid, DATE_DIFF(PARSE_DATE('%Y%m%d',date_event), PARSE_DATE('%Y%m%d', date_first_visit),DAY) AS time, 
-       device, transactions 
-FROM mortality_table ORDER BY transactions DESC, time, fullvisitorid
+       transaction, device FROM mortality_table
